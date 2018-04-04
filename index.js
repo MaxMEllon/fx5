5ff77cb019fa4c478d88d3a531f9066eb9a8ac45
MaxMEllon <maxmellon1994@gmail.com>
2018-04-04 21:20:10 +0900

    Error handling

--- index.js
+++ index.js
@@ -7,15 +7,56 @@ const last = array => {
   return length ? array[length - 1] : void 0
 }
 
+const usage = () => {
+  console.log(`
+Usage:
+
+  WIP
+  `)
+}
+
+const handleError = err => {
+  if (err.lineNumber) {
+    console.error(`${err.lineNumber}:${err.columnNumber}  ${err.message}`)
+  } else {
+    usage()
+  }
+}
+
+const parse = text => {
+  try {
+    const json = JSON5.parse(text)
+    return json
+  } catch (err) {
+    try {
+      const json = JSON.parse(text)
+      return json
+    } catch (err) {
+      handleError(err)
+      process.exit(1)
+    }
+    handleError(err)
+    process.exit(1)
+  }
+}
 
 async function main() {
   const text = await stdin()
-  const json = JSON5.parse(text)
-  const code = last(process.argv)
-
-  const fx5 = eval(code)
-  console.log(fx5(json))
+  let code = process.argv[2]
+  if (!code) code = 'x => x'
+  const json = parse(text)
+  const fx5 = eval(`json => {
+      try {
+        const func = ${code}
+        return func(json)
+      } catch (err) {
+        console.error(err)
+        process.exit(1)
+      }
+    }`)
+  console.log(JSON.stringify(fx5(json), null, 2).replace(/(^")|("$)/g, ''))
 }
 
 main()
+  .catch(err => console.error(err))
 

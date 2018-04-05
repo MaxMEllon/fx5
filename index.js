5d40d9ba0ad7b56e0bc98bc665b7a6febe2546bd
MaxMEllon <maxmellon1994@gmail.com>
2018-04-05 20:04:37 +0900

    Refactoring

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 const stdin = require('get-stdin')
-const { parse, last, transpile } = require('./lib/utils')
+const { parse, last, transpile, generateFx, putToStdout } = require('./lib/utils')
 
 async function main() {
   const text = await stdin()
   const code = transpile(process.argv[2])
   const json = parse(text)
-  const fx5 = eval(`json => {
-      try {
-        const func = ${code}
-        return func(json)
-      } catch (err) {
-        console.error(err)
-        process.exit(1)
-      }
-    }`)
-  const string = JSON.stringify(fx5(json), null, 2)
-  if (string) {
-    console.log(string.replace(/(^")|("$)/g, ''))
-  } else {
-    console.log(string)
-  }
+  const fx5 = generateFx(code)
+  putToStdout(fx5, json)
 }
 
 module.exports = main
